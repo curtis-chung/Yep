cebb21c7776058de0d750284192cd021a329601b
Curtis <curtischung06@gmail.com>
2022-11-22 17:51:24 -0500

    Business and User models completed

---
+++ app/models/business.py
@@ -0,0 +1,55 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Business(db.Model):
+    __tablename__ = 'businesses'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
+    review_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("reviews.id")), nullable=False)
+    business_name = db.Column(db.String(255), nullable=False, unique=True)
+    address = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    postal_code = db.Column(db.String(255), nullable=False)
+    lat = db.Column(db.Float, nullable=False)
+    lng = db.Column(db.Float, nullable=False)
+    phone_number = db.Column(db.String(255), nullable=False, unique=True)
+    web_address = db.Column(db.String(255), nullable=True unique=True)
+    menu_web_address = db.Column(db.String(255), nullable=True unique=True)
+    operating_time = db.Column(db.String(255), nullable=False)
+    business_type = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # relationship attributes
+    user = db.relationship("User", back_populates="businesses")
+
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'review_id': self.review_id,
+            'business_name': self.business_name,
+            'address': self.address,
+            'city': self.city,
+            'state': self.state,
+            'postal_code': self.zip,
+            'lat': self.lat,
+            'lng': self.lng,
+            'phone_number': self.phone_number,
+            'web_address': self.web_address,
+            'menu_web_address': self.menu_web_address,
+            'operating_time': self.operating_time,
+            'business_type': self.business_type,
+            'price': self.price,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +11,17 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # relationship attributes
+    businesses = db.relationship("Business", back_populates="user")
+
+
 
     @property
     def password(self):
@@ -28,6 +37,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
