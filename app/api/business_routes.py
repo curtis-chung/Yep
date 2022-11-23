9484513dedfeff248e5a30605e87dcafbd08ce97
Curtis <curtischung06@gmail.com>
2022-11-23 15:29:15 -0500

    Get and Post routes for business

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -8,13 +8,10 @@ business_routes = Blueprint('business', __name__)
 
 
 """
-Query for all businesses
+Query for all businesses and returns them in a dictionary
 """
 @business_routes.route('')
 def all_businesses():
-    """
-    Query for all transactions and returns them in a list of transaction dictionaries
-    """
     businesses = Business.query.all()
     businesses_dict = {}
 
@@ -22,3 +19,59 @@ def all_businesses():
         businesses_dict[business.to_dict()["id"]] = business.to_dict()
 
     return businesses_dict
+
+
+
+"""
+Query for all businesses owned by current user and returns them in a dictionary
+"""
+@business_routes.route('/my-listings')
+@login_required
+def all_businesses():
+    curr_user = current_user.id
+    businesses = Business.query.filter_by(
+        user_id = int(curr_user)
+    )
+
+    businesses_dict = {}
+
+    for business in businesses:
+        businesses_dict[business.to_dict()["id"]] = business.to_dict()
+
+    return businesses_dict
+
+
+
+"""
+Post a new business
+"""
+@business_routes.route('', methods=["POST"])
+@login_required
+def create_buy_transaction(symbol):
+    curr_user = current_user.id
+    # LookupSymbol = symbol.upper()
+
+    form = BusinessForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = form.data
+
+        # insert data into UserTransactions model
+        new_business = BusinessForm(
+            user_id = int(curr_user),
+            business_name = data["business_name"],
+            address = data["address"],
+            city = data["city"],
+            state = data["state"],
+            postal_code = data["postal_code"],
+            lat = data["lat"],
+            lng = data["lng"],
+            phone_number = data["phone_number"],
+            web_address = data["web_address"],
+            operating_time = data["operating_time"],
+            business_type = data["business_type"],
+            price = data["price"],
+        )
+        db.session.add(new_business)
+        db.session.commit()
+        return new_business.to_dict()
